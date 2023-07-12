4aaa6bd815c042d8bae77e692018843dd081a98b
Setthanan <setthananpanichnok@gmail.com>
2023-07-12 22:05:50 +0700

    FirstCommit

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/no-unknown-property */
+
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn , textVariant } from '../utils/motion'
+
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({index , title , icon }) => {
+  return(
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right","spring",0.5 * index, 0.75)} 
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shawdow-card'>
+
+        <div 
+          options={{
+            max:45,
+            scale: 1,
+            speed: 450
+        }}
+        className='bg-tertiary rounde-[20px] py-5 px-12 min-h-[280px] 
+        flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+
+      </motion.div>
+      <motion.p variants={fadeIn("", "",0.1,1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+        Im a student dreaming to become a software developer with experience
+        from SCB TECH X , a digital company that creates OPEN APIS 
+        and other services for SCB Bank. I am familiar with JavaScript, React
+        ,TypeScript ,Node.js ,HTML and CSS as I have worked on 
+        a Time Tracking Project which is an internal web module created
+        for the ease of routine work for the SE team. Ive also had some
+        experience with on boarding APIs via postman app editing consents, 
+        consumer profiles and etc. Im a fast learner and hard working. 
+        Let works together as Im sure I can provide useful solutions to problems 
+        and brings creative ideas to Life.
+
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service,index ) => (
+          <ServiceCard key={service.title} index={index} {...service}   />
+        ))}
+
+      </div>
+
+    </>
+  )
+}
+
+export default SectionWrapper( About, "about")
---
+++ src/components/Works.jsx
@@ -0,0 +1,100 @@
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { github } from "../assets"
+import { SectionWrapper } from "../hoc"
+import { projects } from "../constants"
+import { fadeIn , textVariant } from '../utils/motion'
+
+//data of the projects are in the file constants
+//Note that the data of the project can be changed in the constants file
+//be sure all elements passed in as parameters are properly filled
+
+const ProjectCard = ( { index , name , description , tags , image , source_code_link }) => {
+  return(
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[300px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img 
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div 
+              onClick={() => window.open
+              (source_code_link,"_blank")}
+              className="black-gradient w-10 h-10
+              rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5 ">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+
+        </div>
+
+      </Tilt>
+    </motion.div>
+  )
+}
+
+
+
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p 
+          variants={fadeIn("","",0.1,1)} 
+          className='mt-3 text-secodnary text-[17px] max-w-3xl leading-[30px]' 
+        >
+          These following projects showcases the skills and experience I have faced
+          and accomplished both with the help of my mentors and on my own throughout my whole Internship period.
+          The ability to manage time and projects effectively, at the same time complete them using 
+          different technologies that requires re-learning ability consistently.
+        </motion.p>
+
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project,index) => (
+          <ProjectCard key={`project-${index} `} {...project} index={index} />
+        ))}
+
+      </div>
+      
+    </>
+  )
+}
+
+export default SectionWrapper(Works , "")
