7f6167f51f1c2d262756843046e91d1301ef50de
Sin-cy <setthananpanichnok@gmail.com>
2024-06-06 03:13:02 +0700

    removed ComputerCanvas and circle gradient design line
    Webpage crashing on mobile devives after adding <StarCanvas /> to the
    whole page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,32 +4,35 @@ import { styles } from '../styles'
 import { ComputersCanvas } from './canvas'
 
 //915eff
+//<p className="flex flex-row justify-end sm:text-yellow-400 my-5 lg:text-yellow-400">"Try moving the galaxy"</p> 
+
+
+//Remove gradient circle line div for now 
+//<div className='w-4 h-4 rounded-full bg-[#facc15]'/>
+//    <div className='w-1 sm:h-80 h-40 violet-gradient'/>
 
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-4 h-4 rounded-full bg-[#facc15]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+
         </div>
 
         <div>
                 <h1 className={`${styles.heroHeadText} text-white`}>
                     Setthanan <span className='text-[#facc15]'>Pheno</span>
                 </h1>
-                <p className={`${styles.heroSubText} mt-5 drop-shadow-3xl bg-clip-text text-amber-400 backdrop-blur-[2px] backdrop-brightness-100  backdrop-contrast-120  `}>
+                <p className={`${styles.heroSubText} mt-10 drop-shadow-3xl bg-clip-text text-amber-400 backdrop-blur-[2px] backdrop-brightness-100  backdrop-contrast-120  `}>
                     I am a self-motivated engineering student,<br className='sm:block hidden'/> 
                     committed to evolving into a highly skilled software engineer. My journey entails refining my skills and broadening my experiences, 
                     drawing from invaluable knowledge imparted by a highly skilled team, mentors, and my personal research endeavors.
                 </p>
-                <p className="flex flex-row justify-end sm:text-yellow-400 my-5 lg:text-yellow-400">"Try moving the galaxy"</p> 
         </div>
 
 
       </div>
 
-      <ComputersCanvas />
       
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
