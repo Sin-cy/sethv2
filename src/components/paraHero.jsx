b8a85ad6b609892df7ef5ce4484997d7eb01818a
Sin-cy <setthananpanichnok@gmail.com>
2024-06-11 10:02:23 +0700

    Final Animation update

--- src/components/paraHero.jsx
+++ src/components/paraHero.jsx
@@ -10,13 +10,13 @@ const ParaHero = () => {
 
             <div className="parallax_content absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 ">
                 <div>
-                    <h1 className={`${styles.heroHeadText} text-[#facc15] mt-7 `}>
+                    <h1 className={`${styles.heroHeadText} text-[#facc15] mt-14 `}>
                         Setthanan <span className='text-[#facc15]'>Pheno</span>
                     </h1>
 
                     <Position />
 
-                    <p className={`${styles.heroSubText} mt-24 drop-shadow-3xl bg-clip-text text-amber-400 backdrop-blur-[2px] backdrop-brightness-30  backdrop-contrast-120 `}>
+                    <p className={`${styles.heroSubText} mt-16 drop-shadow-3xl bg-clip-text text-amber-400 backdrop-blur-[2px] backdrop-brightness-30  backdrop-contrast-120 `}>
                         I am a self-motivated engineering student, <br className='sm:block hidden'/> 
                     committed to evolving into a highly <br className='sm:block hidden'/> skilled software engineer. 
                     </p>
